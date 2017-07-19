a4d00741abccf8e434e805f626b0283d12a224d7
junior <junior_a87hotmail.com>
2017-07-19 10:52:48 +0200

    added try for import of Tkinter to allow for python 2 and 3

--- boo.py
+++ boo.py
@@ -3,13 +3,16 @@ Created on Jul 9, 2017
 
 @author: junior
 '''
-from argparse import Action
+
 from _functools import partial
 import sys
-from Tkinter import *
-print ("Hello world!")
-import Tkinter as tk
-
+try:
+    from Tkinter import *
+    import Tkinter as tk
+except ImportError:
+    from tkinter import *
+    import tkinter as tk
+    
 '''
 top = tk.Tk()
 
@@ -26,11 +29,14 @@ class Application(tk.Frame):
         self.createWidgets()
         
     def createWidgets(self):
+        lbstr = StringVar()
+        lbstr.set("Make me change")
+        
         self.quitButton = tk.Button(self, text="Buzz OFF", command = self.quit)
         self.quitButton.grid()
         
         self.btn = tk.Button(self, text="dont know about this", command=partial(action, "boo"))
-        self.btn.grid(row=0, column=1, columnspan=5)
+        self.btn.grid(row=0, column=2, columnspan=2)
         
         self.btn2 = tk.Button(self,  text="Grab text", command = getInput)
         self.btn2.grid(row=1, column=0, columnspan=2)
@@ -41,14 +47,21 @@ class Application(tk.Frame):
         self.Txt = tk.Text(self, height=2, width=30)
         self.Txt.grid(row=1, column=2, columnspan=1)
         
+        self.lb = Label(self, text=lbstr)
+        self.lb.grid(row=0, column=1)
+        
+        print(lbstr)
+        
     def txtGet(self):
         return self.Txt.get("1.0", 'end-1c') 
         #the 1.0 means read from line 1 0th char
         #end-1c means read to end but do not read the last char (-1c) since this is an extra \n
-    
-            
         
-
+    
+    def changeLabel(self, strinput):
+        self.lbstr.set(strinput)
+           
+        
 def getText():
         content = app.txtGet()
         print (content)
@@ -59,15 +72,19 @@ def action( strn ):
     return
     
 def getInput():
-    try:
-        stri = sys.stdin.readline()
-        print (stri)
+    #try:
+    stri = sys.stdin.readline()
+    #stri = input("write text to print")
+    print("grabbing stdin")
+    app.changeLabel(stri)
+    print(stri)
+    print("grab finished")
+    '''    
     except:
         print("no input detected!")
-    
+    '''
     return
 
-    
 app = Application()
 app.master.title("I do Nothing")
 app.mainloop()
